a063b1174ead1d950ba2935d24e2a8b8b2cdda7b
Ganesh Acharya <ganesh.a@juegostudio.com>
2022-03-18 18:09:58 +0530

    final product

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,47 @@
 import './ExploreContainer.css';
 
+import {
+  SignInWithApple,
+  SignInWithAppleResponse,
+  SignInWithAppleOptions,
+} from '@capacitor-community/apple-sign-in';
+
 interface ContainerProps { }
 
+let options: SignInWithAppleOptions = {
+  clientId: process.env.clientId || '',
+  redirectURI: process.env.clieredirectURIntId || '',
+  scopes: process.env.scopes || '',
+  state: process.env.state || '',
+  nonce: process.env.nonce || '',
+};
+
 const ExploreContainer: React.FC<ContainerProps> = () => {
+
+  const handleAppleLogin = (e: any) => {
+    SignInWithApple.authorize(options)
+    .then((result: SignInWithAppleResponse) => {
+      fetch((process.env.endpoint||''), {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(result)
+      }).then(res => res.json())
+      .then(data => {
+        alert("All ok!!!")
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }
+
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <button onClick={handleAppleLogin}>Apple Login</button>
     </div>
   );
 };
---
+++ src/env/env.dev.js
@@ -0,0 +1,8 @@
+export const ENV = {
+    clientId: 'com.nomostimebank.demo',
+    redirectURI: 'https://f4c5-2405-204-502e-fbb6-1caa-d3-e82c-33f9.ngrok.io/apple/back',
+    scopes: 'email name',
+    state: '12345',
+    nonce: 'nonce',
+    endpoint: 'https://f4c5-2405-204-502e-fbb6-1caa-d3-e82c-33f9.ngrok.io/logs'
+};
